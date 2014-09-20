64c85cba4899c64a4b5ce97aa2c83f936dded23c
Maxime Mouchet <max@maxmouchet.com>
2014-09-20 14:13:50 +0200

    Fix accents in SIT131 BE1 Ex1

--- SIT131/BE1/exercice1.m
+++ SIT131/BE1/exercice1.m
@@ -3,20 +3,20 @@ close all;
 
 % x = nombre de transformations
 % y = nombre de placements
-% z = nombre de touch?s
+% z = nombre de touches
 
 % x + 3y + 6z = 35
 % x -  y +  z = 0
 % x +  y +  z = 10
 
-% A: matrice correspondant au syst?me d'?quations
+% A: matrice correspondant au systeme d'equations
 A = [ 1 3 6; 1 -1 1; 1 1 1];
 
-% B: matrice du r?sultat des ?quations
+% B: matrice du resultat des equations
 B = [ 35; 0; 10 ];
 
-% X: solution du syst?me
+% X: solution du systeme
 X = inv(A)*B
 
-% La solution est unique car le d?terminant de A != 0
+% La solution est unique car le determinant de A != 0
 det = det(A)
