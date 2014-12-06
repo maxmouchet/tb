6b296cefc4a95fa5e9f5f6a11ee767e488787ab9
Maxime Mouchet <max@maxmouchet.com>
2014-12-06 22:43:31 +0100

    Add plotting script :chart_with_upwards_trend:

--- SIT151/C/TP2/tools/benchmark.sh
+++ SIT151/C/TP2/tools/benchmark.sh
@@ -17,10 +17,18 @@ fi
 ALGORITHM=$1
 shift
 
+OUTPUT_FILE=$ALGORITHM".txt"
+echo '' > $OUTPUT_FILE
+
 printf "%-15s%-10s%s\n" File Words Seconds
 
 for file in $@; do
+    word_count=`wc -w $file | grep -Eo '\d+\s'`
+    running_time=$( { $TIME $FLAGS $ALGORITHM $file >/dev/null; } 2>&1 )
+    
     printf "%-15s" $file
-    printf "%-10s" `wc -w $file | grep -Eo '\d+\s'`
-    $TIME $FLAGS $ALGORITHM $file >/dev/null
+    printf "%-10s" $word_count
+    printf "%s\n" $running_time
+
+    echo "$word_count $running_time" >> $OUTPUT_FILE
 done
