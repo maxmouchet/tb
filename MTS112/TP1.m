3543c480c26ec79287375e92f04240eac965fb45
Maxime Mouchet <max@maxmouchet.com>
2015-06-05 16:17:55 +0200

    Update Matlab code

--- MTS112/TP1.m
+++ MTS112/TP1.m
@@ -1,34 +1,37 @@
 close all;
 clear all;
 
-% FIR
+FIR
 Fe = 16000;
 Fc = 2000;
 
 wn = Fc/(Fe/2);
 
-N = 7;
+N = 19;
 
-h_8_ham = fir1(N, wn, hamming(N+1))
+h_8_ham = fir1(N, wn, hamming(N+1));
+[H_8_Ham, f] = freqz(h_8_ham, 1, 512, 'whole', Fe);
 
-A = [1];
-B = h_8_ham;
-
-[H_8_Ham, f] = freqz(A, B, 512, 'whole', Fe);
+h_8_box = fir1(N, wn, boxcar(N+1))
+[H_8_Box, g] = freqz(h_8_box, 1, 512, 'whole', Fe)
 
 figure('Color',[1 1 1]);
 
 subplot(2,2,1)
-stem(h_8_ham)
+plot((f/(2*pi))-(1/2), abs(H_8_Ham))
+title('N = 19, Hamming. Module de la reponse frequentielle')
 
 subplot(2,2,2)
-plot(f, abs(H_8_Ham))
+plot((f/(2*pi))-(1/2), 20*log10(abs(H_8_Ham)))
+title('N = 19, Hamming. Module de la reponse frequentielle (dB)')
 
 subplot(2,2,3)
-semilogy(f, fftshift(20*log10(abs(H_8_Ham))))
+plot((g/(2*pi))-(1/2), abs(H_8_Box))
+title('N = 19, Boxcar. Module de la reponse frequentielle')
 
 subplot(2,2,4)
-plot(f, fftshift(unwrap(angle(H_8_Ham))))
+plot((g/(2*pi))-(1/2), 20*log10(abs(H_8_Box)))
+title('N = 19, Boxcar. Module de la reponse frequentielle (dB)')
 
 clear all;
 
@@ -45,11 +48,35 @@ delta2 = 30
 
 figure('Color',[1 1 1]);
 
+subplot(2,1,1)
+plot((f/(2*pi))-(1/2), 20*log10(abs(H)))
+title('Module de la reponse frequentielle (dB)')
+
+subplot(2,1,2)
+plot((f/(2*pi))-(1/2), unwrap(angle(H)))
+title('Phase de la reponse frequentielle')
+
+% Decomposition en cellules du 1er et du 2eme ordre
+Z = zp2sos(roots(A), roots(B), A(1))
+
+
+[H_1, f] = freqz(Z(1,1:3), Z(1,4:6), 512, 'whole', Fe);
+[H_2, g] = freqz(Z(2,1:3), Z(2,4:6), 512, 'whole', Fe)
+
+figure('Color',[1 1 1]);
+
 subplot(2,2,1)
-semilogy(f, fftshift(20*log10(abs(H))))
+plot((f/(2*pi))-(1/2), 20*log10(abs(H_1)))
+title('Cellule 1. Module de la reponse frequentielle (dB)')
 
 subplot(2,2,2)
-plot(f, fftshift(unwrap(angle(H))))
+plot((f/(2*pi))-(1/2), unwrap(angle(H_1)))
+title('Cellule 1. Phase de la reponse frequentielle')
 
-% D?composition en cellules du 1er et du 2eme ordre
-Z = zp2sos(roots(A), roots(B), A(1))
+subplot(2,2,3)
+plot((g/(2*pi))-(1/2), 20*log10(abs(H_2)))
+title('Cellule 2. Module de la reponse frequentielle (dB)')
+
+subplot(2,2,4)
+plot((g/(2*pi))-(1/2), unwrap(angle(H_2)))
+title('Cellule 2. Phase de la reponse frequentielle')
